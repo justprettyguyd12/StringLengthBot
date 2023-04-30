92a3c62709cf9964ac0d3a5a51a30d8bfedeb01c
Danila Shalyutin <danshalutin@gmail.com>
2023-04-30 23:01:03 +0400

    deploy version

--- handlers.py
+++
@@ -1,9 +0,0 @@
-from main import bot, dp
-
-from aiogram.types import Message
-
-
-@dp.message_handler()
-async def echo(message: Message):
-    text = f"{message.text}"
-    await bot.send_message(chat_id=message.from_user.id, text=len(text))
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, executor
-from config import TOKEN
+from aiogram.types import Message
+
+
+TOKEN = '5443248088:AAGSDReJtJ5VYowDLzmD-0v84kbs6skt1Zs'
 
 loop = asyncio.get_event_loop()
 bot = Bot(TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, loop=loop)
 
+
+@dp.message_handler()
+async def echo(message: Message):
+    text = f"{message.text}"
+    await bot.send_message(chat_id=message.from_user.id, text=len(text))
+
 if __name__ == "__main__":
-    from handlers import dp
     executor.start_polling(dp)
 
 
